ba80fcf26fbd42cc0f70c0fdb5eeb8866f8eb229
Antonin Lefevre <antonin_08430@hotmail.fr>
2017-09-05 11:04:27 +0200

    Finalisation du pattern par defaut en attendant des test sur divers projet

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const port = require('./config/server').port;
+
 let http = require('http');
+let url = require('url');
 let express = require('express');
+
 let logger = require('morgan');
+let router = require('./router');
 
 let app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+// Gestion des vues avec le moteur de template EJS
+app.set('views', 'views');
 app.set('view engine', 'ejs');
+app.set('port', port);
+
+// Log avec morgan
+app.use(logger('dev'));
+app.use('/assets', express.static('public'));
+app.use('/', router);
+
+
+let server = http.createServer(app);
+
+server.listen(port);
